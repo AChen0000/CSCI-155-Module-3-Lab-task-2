2c08d7fd87443c7de458bfdf42e5575a2bc9f173
AChen0000 <143315959+AChen0000@users.noreply.github.com>
2025-09-29 23:06:22 -0400

    Update LinearSolver.java

--- LinearSolver.java
+++ LinearSolver.java
@@ -1,32 +1,39 @@
 import java.util.Scanner;
 
 public class LinearSolver {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Enter the values 
-        System.out.print("Enter a, b, c, d, e, f: ");
-        double a = input.nextDouble();
-        double b = input.nextDouble();
-        double c = input.nextDouble();
-        double d = input.nextDouble();
-        double e = input.nextDouble();
-        double f = input.nextDouble();
+        while (true) { //  have loop 
+            // Enter the values 
+            System.out.print("Enter a, b, c, d, e, f (or type 0 0 0 0 0 0 to quit): ");
+            double a = input.nextDouble();
+            double b = input.nextDouble();
+            double c = input.nextDouble();
+            double d = input.nextDouble();
+            double e = input.nextDouble();
+            double f = input.nextDouble();
 
-        // Find denominator (ad - bc)
-        double denominator = a * d - b * c;
+            // Exit 
+            if (a == 0 && b == 0 && c == 0 && d == 0 && e == 0 && f == 0) {
+                System.out.println("Goodbye!");
+                break; // leave the loop
+            }
 
-        // Check if denominator is 0
-        if (denominator == 0) {
-            // If the denominator is 0, the equations cannot be solved
-            System.out.println("The equation has no solution");
-        } else {
-            // If the denominator is not 0, find x and y
-            double x = (e * d - b * f) / denominator;
-            double y = (a * f - e * c) / denominator;
+            // Find the denominator (ad - bc)
+            double denominator = a * d - b * c;
 
-            // Show the results
-            System.out.println("x is " + x + " and y is " + y);
+            // Check if denominator is 0
+            if (denominator == 0) {
+                System.out.println("The equation has no solution");
+            } else {
+                // Find x and y
+                double x = (e * d - b * f) / denominator;
+                double y = (a * f - e * c) / denominator;
+
+                // Show the results
+                System.out.println("x is " + x + " and y is " + y);
+            }
         }
 
         // Finish
